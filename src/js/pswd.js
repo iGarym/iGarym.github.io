4142422d0e9a5912e244105a32f30767b6e87a63
gary <igyueming@gmail.com>
2016-07-18 16:28:36 +0800

    change font-size(rem)

--- src/js/pswd.js
+++ src/js/pswd.js
@@ -1,6 +1,6 @@
 var dpr = window.devicePixelRatio || 1,
     fontEl = document.createElement('style'),
-    rem = document.documentElement.clientWidth / 100;
+    rem = document.documentElement.clientWidth / 1;
 // 设置data-dpr属性，留作的css hack之用
 document.documentElement.setAttribute('data-dpr', dpr);
 // 动态写入样式
