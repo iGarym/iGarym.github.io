caaa93239f4e4f7f06efd50c316bc830cc8502a6
gary <igyueming@gmail.com>
2016-07-18 19:19:05 +0800

    change path

---
+++ source/fis-conf.js
@@ -0,0 +1,76 @@
+//发布到指定目录
+fis.match('(*.{png,gif,jpg})', {release: '/src/images/$1$2$3'});
+fis.match('/src/js/(*.{js,jsx})', { release: '/src/js/$1$2' });
+fis.match('/src/css/(*.{css,less})', { release: '/src/css/$1$2'});
+fis.match('/src/fonts/(*.{ttf,woff})', { release: '/src/fonts/$1$2'});
+fis.match('/views/**.html',{release: '/$0'});
+fis.match('index.html',{release: '/$0'});
+/*fis.match('/test/*.json', {release: '$0'});
+fis.match('/test/*.conf', {release: '/config/server.conf'});*/
+//加MD5
+fis.match('*.{js,css,png,jpg,gif,jpeg}', {
+    useHash: false
+});
+
+//编译LESS
+/*fis.match('*.less', {
+    parser: fis.plugin('less'),
+    // .less 文件后缀构建后被改成 .css 文件
+    rExt: '.css'
+})*/
+
+//压缩资源
+fis.match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+});
+fis.match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+});
+fis.match('*.css', {
+    optimizer: fis.plugin('clean-css')
+})
+
+//图片合并
+fis.match('::package', {
+    spriter: fis.plugin('csssprites')
+})
+fis.match('(*.{css,less})', {
+    //给匹配到的文件分配属性 `useSprite`
+    useSprite: true
+});
+fis.config.set('settings.spriter.csssprites', {
+    //图之间的边距
+    margin: 20,
+    //使用矩阵排列方式，默认为线性`linear`,matrix
+    layout: 'linear'
+});
+
+
+//文件合并
+/*fis.match('::package', {
+  postpackager: fis.plugin('loader')
+});
+fis.match('*.less', {
+  parser: fis.plugin('less'),
+  rExt: '.css'
+});
+
+fis.match('*.{less,css}', {
+  packTo: '/static/aio.css'
+});*/
+
+//debug
+/*fis.media('debug').match('*.{js,css,png,}', {
+    useHash: false,
+    useSprite: false,
+    optimizer: null
+})*/
+
+//后台假数据处理
+/*fis.match('/test/**', {
+  release: '$0'
+});
+fis.match('/test/server.conf', {
+  release: '/config/server.conf'
+});
+*/
--- src/js/pswd.js
+++ src/js/pswd.js
@@ -1,8 +1,1 @@
-var dpr = window.devicePixelRatio || 1,
-    fontEl = document.createElement('style'),
-    rem = document.documentElement.clientWidth / 10;
-// 设置data-dpr属性，留作的css hack之用
-document.documentElement.setAttribute('data-dpr', dpr);
-// 动态写入样式
-document.documentElement.firstElementChild.appendChild(fontEl);
-fontEl.innerHTML = 'html{font-size:' + rem + 'px!important;}';
+var dpr=window.devicePixelRatio||1,fontEl=document.createElement("style"),rem=document.documentElement.clientWidth/10;document.documentElement.setAttribute("data-dpr",dpr),document.documentElement.firstElementChild.appendChild(fontEl),fontEl.innerHTML="html{font-size:"+rem+"px!important;}";
