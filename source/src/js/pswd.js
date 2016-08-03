c76eaad754ae9bab1d642826b6f8b4909e2773ea
gary <igyueming@gmail.com>
2016-08-03 23:58:03 +0800

    bug fixed

--- source/src/js/pswd.js
+++ source/src/js/pswd.js
@@ -3,13 +3,12 @@
     var input = document.querySelector('.p-main input[type=text]');
     var btn = document.querySelector('.p-main button');
     var resultNode = document.querySelector('.p-main p');
-    var radioVal = document.querySelector('input:checked').value;
     btn.addEventListener('click', function(e) {
         var _val = md5(input.value);
         var _tmp = _val.replace(/\d{2,}/g, function(e) {
             return Number(e).toString(16)
         });
-        var _typeVal = document.querySelector('input[type=radio]:checked').value();
+        var _typeVal = document.querySelector('input[type=radio]:checked').value;
         var _len = (_typeVal === '2' ? 12 : 9);
         resultNode.innerHTML = _tmp.substr(0 - _len);
     }, false);
